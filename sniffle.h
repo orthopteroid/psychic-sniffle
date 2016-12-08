5534562e6bd370cd2f6719e0bdcd80aa20c69c84
John Howard <orthopteroid@gmail.com>
2016-12-07 21:45:09 -0800

    - pre-build a prefix table for byte-distributions and phenotype-expected values - factored out repeated calls to 'sample' and replaced with a lookup into a prefix-table - templated splice<> to take a uint size value

--- sniffle.h
+++ sniffle.h
@@ -14,6 +14,7 @@
 #include <cstring>
 #include <omp.h>
 #include <functional>
+#include <assert.h>
 
 #include "taus88.h"
 
@@ -21,32 +22,61 @@
 
 namespace sniffle {
 
-template<typename T>
-uint sample(T *arr, int samples, uint32_t randValue) {
-    T target = arr[samples - 1] * ((float_t) randValue / (float_t) UINT32_MAX);
-    for (int i = 0; i < samples; i++)
-        if (arr[i] >= target) return i;
-    throw new std::runtime_error("sample overflow");
-    return 0;
+// Performs a normalization and a prefix sum on the input array.
+// Input and output arrays can use mismatched numeric types.
+template<typename OT, uint ON, typename IT, uint IN>
+int buildSamplerTable(OT* outArr, IT* inArr)
+{
+    IT min = inArr[0];
+    for(int i=1; i<IN; i++) {
+        min = std::min(min, inArr[i]);
+    }
+
+    // compute area
+    float_t sum = 0;
+    for(int i=0; i<IN; i++) {
+        sum += (inArr[i] - min);
+    }
+
+    // when all equal, return a uniform distr
+    if (sum == 0) {
+        for(int i=0; i<IN; i++)
+            outArr[i] = i;
+        return IN;
+    }
+
+    // pick a scaling coef such that output outArr is filled
+    float_t coef = (float)(ON -1) / sum;
+
+    // otherwise return a nonuniform distribution
+    uint i = 0;
+    for (uint j = 0; j < IN; j++) {
+        uint expectedValue = ( (float_t)(inArr[j]) - (float_t)min ) * coef;
+        for (uint k = 0; k < expectedValue; k++)
+            outArr[i++] = j;
+    }
+
+    if (i == 0 || i > ON)
+        throw new std::runtime_error("outArr error");
+
+    return i;
 }
 
-template<typename T1, typename T2>
-void accumulate(T1 *arrOut, T2 *arr, int samples) {
-    T1 sum = 0;
-    for (int i = 0; i < samples; i++)
-        arrOut[i] = (sum += arr[i]);
-}
-
-void splice(uint8_t *out, uint8_t *a, uint8_t *b, uint uSize, uint uBit) {
+// Splices two byte arrays of the same length together at the specified bit.
+// When splicing, bits from a will be written to lower memory than bits from b.
+// In the transition byte, high bits come from a and high bits come from b. Does it matter?
+template<uint Size>
+void splice(uint8_t *out, uint8_t *a, uint8_t *b, uint uRand) {
     const uint8_t u8Mask[] = {
             0b00000000, 0b00000001, 0b00000011, 0b00000111,
             0b00001111, 0b00011111, 0b00111111, 0b01111111
     };
 
+    uint uBit = uRand % ( Size * 8 );
     int i = 0;
     while (i < (uBit / 8)) out[i++] = a[i];
     out[i++] = (a[i] & ~u8Mask[uBit & 7]) | (b[i] & u8Mask[uBit & 7]);
-    while (i < uSize) out[i++] = b[i];
+    while (i < Size) out[i++] = b[i];
 }
 
 //////////////////////////////////
@@ -58,7 +88,8 @@ struct ByteAnalyser {
     uint8_t *GetByteArr(StateType &state) { return (uint8_t *) &state; }
 
     uint8_t distr[StateSize][256];
-    uint32_t dSum[StateSize][256];
+    uint8_t dSampler[StateSize][65535];
+    uint16_t dSamplerN[StateSize];
 
     void dumpStats() {
         for (int ss = 0; ss < StateSize; ss++) {
@@ -93,8 +124,10 @@ struct ByteAnalyser {
 
         // recalc
 #pragma omp parallel for
-        for (int ss = 0; ss < StateSize; ss++)
-            accumulate(&(dSum[ss][0]), &(distr[ss][0]), 256);
+        for (int ss = 0; ss < StateSize; ss++) {
+            dSamplerN[ss] = buildSamplerTable<uint8_t, 65535, uint8_t, 256>(&(dSampler[ss][0]), &(distr[ss][0]));
+        }
+
     }
 
     void reset() {
@@ -102,15 +135,24 @@ struct ByteAnalyser {
         // on the convergence rate. A higher value will require more attenuation
         // cycles before the S/R ratio get stronger.
         memset(distr, UINT8_MAX >> 2, StateSize * 256); // a uniform distribution
+
+#pragma omp parallel for
+        for (int ss = 0; ss < StateSize; ss++) {
+            for (int i = 0; i < 256; i++)
+                dSampler[ss][i] = i; // a uniform distribution
+            dSamplerN[ss] = ~0;
+        }
     }
 
-    void mutatebyte(uint8_t *p, int byte, uint32_t randValue) {
-        p[byte] = sample(&(dSum[byte][0]), 256, randValue); // aka jumping mutation
+    void mutatebyte(uint8_t *p, std::function<uint32_t()> fnRand) {
+        int byte = fnRand() % StateSize;
+        p[byte] = dSampler[byte][ fnRand() % dSamplerN[byte] ];
     }
 
     void randomize(uint8_t *p, std::function<uint32_t()> fnRand) {
-        for (int ss = 0; ss < StateSize; ss++)
-            p[ss] = sample(&(dSum[ss][0]), 256, fnRand()); // synthesis using the byte-distributions
+        for (int ss = 0; ss < StateSize; ss++) {
+            p[ss] = dSampler[ss][ fnRand() % dSamplerN[ss] ];
+        }
     }
 };
 
@@ -133,10 +175,9 @@ struct Maximizer {
 
     StateType state[2][Population];
 
-    float_t fSum[Population];
     float_t e[Population];
-    float_t eSum0[Population]; // includes all items, even item 0
-    float_t eSum1[Population - 1]; // does not include item 0. eSum1[0] == e[1]
+    uint16_t eSampler[65535];
+    uint16_t eSamplerN;
 
     ByteAnalyser<StateType> byteAnalyser;
     Taus88State taus88State;
@@ -184,26 +225,28 @@ struct Maximizer {
         dumpStats();
 #endif
 
-        accumulate(fSum, f, Population);
+        // find max, and clobber it to prevent saturation
+        int imax = 0;
+        int imin = 0;
+        for (int i = 1; i < Population; i++) {
+            if (f[i] > f[imax]) imax = i;
+            if (f[i] < f[imin]) imin = i;
+        }
+        for (int i = 1; i < Population; i++) {
+            if (f[i] == f[imax]) f[i] = f[imin];
+        }
 
-        // calc expected value
-#pragma omp for
-        for (int p = 0; p < Population; p++)
-            e[p] = f[p] / fSum[Population - 1];
-
-        // calc cumulative prob density
-        accumulate(eSum0, &(e[0]), Population);
-        accumulate(eSum1, &(e[1]), Population - 1); // note: eSum1 doesn't account for item 0
+        // calc sampler table
+        eSamplerN = buildSamplerTable<uint16_t, 65535, float_t, Population>(eSampler, f);
 
         // sample elites
-        eliteSamples[0] = 0; // add best only once to prevent saturation
+        eliteSamples[0] = imax; // add best only once to prevent saturation
 #pragma omp parallel
         {
             Taus88 taus88(taus88State);
 #pragma omp for
             for (int i = 1; i < EliteSamples; i++)
-                eliteSamples[i] =
-                        sample(eSum1, Group3End - 1, taus88()) + 1; // note: eSum1 doesn't account for item 0
+                eliteSamples[i] = eSampler[ taus88() % eSamplerN ];
         }
 
         // refresh distributions
@@ -212,10 +255,6 @@ struct Maximizer {
         /////////////////////////////
         // build next generation
 
-        // find and keep best
-        int imax = 0;
-        for (int i = 1; i < Population; i++)
-            if (f[i] > f[imax]) imax = i;
         memcpy(newPop(0), oldPop(imax), (uint) StateSize);
 
 #pragma omp parallel
@@ -224,34 +263,35 @@ struct Maximizer {
 #pragma omp for nowait
             for (int i = 1; i < Group2End; i++) {
                 // g2: preserve elites
-                int p = sample(eSum1, Population - 1, taus88()) + 1; // +1 to skip item 0 and avoid saturation
+                int p = eSampler[ taus88() % eSamplerN ];
                 memcpy(newPop(i), oldPop(p), (uint) StateSize);
             }
 #pragma omp for nowait
             for (int i = Group2End +1; i < Group3End; i++) {
                 // g3: semi-preserve elites
-                int p = sample(eSum1, Population - 1, taus88()) + 1; // +1 to skip item 0 and avoid saturation
+                int p = eSampler[ taus88() % eSamplerN ];
                 memcpy(newPop(i), oldPop(p), (uint) StateSize);
-                byteAnalyser.mutatebyte(newPop(i), taus88() % StateSize, taus88());
+                std::function<uint32_t()> fRand = [&taus88]() -> uint32_t { return taus88.operator()(); };
+                byteAnalyser.mutatebyte(newPop(i), fRand);
             }
 #pragma omp for nowait
             for (int i = Group3End +1; i < Group4End; i++) {
                 // g4: some favourables are spliced with best
-                int b = sample(eSum1, Population - 1, taus88()) + 1; // +1 to skip item 0 and avoid saturation
-                splice(newPop(i), oldPop(0), oldPop(b), (uint) StateSize, (uint) taus88() % (8 * StateSize));
+                int b = eSampler[ taus88() % eSamplerN ];
+                splice<StateSize>(newPop(i), oldPop(0), oldPop(b), (uint) taus88());
             }
 #pragma omp for nowait
             for (int i = Group4End +1; i < Group5End; i++) {
                 // g5: some favourables are spliced with best (other way)
-                int a = sample(eSum1, Population - 1, taus88()) + 1; // +1 to skip item 0 and avoid saturation
-                splice(newPop(i), oldPop(a), oldPop(0), (uint) StateSize, (uint) taus88() % (8 * StateSize));
+                int a = eSampler[ taus88() % eSamplerN ];
+                splice<StateSize>(newPop(i), oldPop(a), oldPop(0), (uint) taus88());
             }
 #pragma omp for nowait
             for (int i = Group5End +1; i < Group6End; i++) {
                 // g6: favourables that are only spliced
-                int a = sample(eSum1, Population - 1, taus88()) + 1; // +1 to skip item 0 and avoid saturation
-                int b = sample(eSum1, Population - 1, taus88()) + 1; // +1 to skip item 0 and avoid saturation
-                splice(newPop(i), oldPop(a), oldPop(b), (uint) StateSize, (uint) taus88() % (8 * StateSize));
+                int a = eSampler[ taus88() % eSamplerN ];
+                int b = eSampler[ taus88() % eSamplerN ];
+                splice<StateSize>(newPop(i), oldPop(a), oldPop(b), (uint) taus88());
             }
 #pragma omp for nowait
             for (int i = Group6End +1; i < Population; i++) {
